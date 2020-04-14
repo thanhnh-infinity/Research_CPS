530479d847966544a56992a0cf9a9ef592deaff0
Thanh Nguyen <thanhnh2911@gmail.com>
2020-04-14 12:15:25 -0600

    update

--- src/asklab/querypicker/QUERIES/generate-all-lp-files.sh
+++ src/asklab/querypicker/QUERIES/generate-all-lp-files.sh
@@ -2,7 +2,7 @@
 
 DIRS="EX0-general EX0-optimistic EX0-pessimistic EX0-pessimistic-prob \
       EX0-related-req EX0-sake-of-concern EX0-skeptical \
-      EX1-camera EX2-laneassist EX3-adaptivecruise"
+      EX1-camera EX2-laneassist EX3-adaptivecruise EX4-elevator"
 
 for d in $DIRS
 do
