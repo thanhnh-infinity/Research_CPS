63e318b5c96efaef2bd322edf11ed12450a2cb00
Matt Bundas <bundasma@msu.edu>
2020-05-26 13:09:18 -0600

    added a few features, scrolling in y direction, zoom button, different colors for concerns and properties

--- interface/testing/object_gui.py
+++ interface/testing/object_gui.py
@@ -2,7 +2,7 @@ from script_networkx import draw_ontology
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from script_networkx import draw_ontology
-
+import numpy as np
 
 import tkinter as tk
 import tkinter.font as tkFont
@@ -14,101 +14,115 @@ from owlready2 import *
 class OntologyGUI:
     
     def __init__(self,master):
-        self.master = master
         
-    
+        self.zoom = 1
+        self.fontsize = 8
+        
+        self.master = master
         
         master.title("Ontology GUI")
           
+        #set up main canvas
         self.canvas = Canvas(master, height = 1200, width = 2000, bg = "#18453b")
         self.canvas.pack()
         
-        
+        #set up title text
         self.header_frame = Frame(master,bg ="#18453b" )
         self.header_frame.place(relwidth = .8, relheight = .06, relx = .1, rely = 0.02)
         self.header_text = Label(self.header_frame, text="CPS Ontology Visualization",fg = "white",bg = "#18453b", font = "Helvetica 30 bold italic")
         self.header_text.pack()
         
+        #set up footer text
         self.footer_frame = Frame(master,bg ="#18453b")
         self.footer_frame.place(relwidth = .4, relheight = .2, relx = .61, rely = 0.9)
         self.footer_text = Label(self.footer_frame, text="Matt Bundas, Prof. Son Tran, Thanh Ngyuen, Prof. Marcello Balduccini",fg = "white",bg = "#18453b", font = "Helvetica 8 bold italic", anchor = "e")
         self.footer_text.pack()
 
-        self.frame = Frame(master, bg="white")
-        self.frame.place(relwidth = .2, relheight = .8, relx = .01, rely = 0.1)
+        #set up frame on left for inputs
+        self.input_frame = Frame(master, bg="white")
+        self.input_frame.place(relwidth = .2, relheight = .8, relx = .01, rely = 0.1)
 
-
-
-            
-        self.inputPrompt = Label(self.frame, text = "Input ontology", font = fontStyle,fg= "#747780",bg = "white")
+        #set up prompt/entry for input ontology
+        self.inputPrompt = Label(self.input_frame, text = "Input ontology", font = fontStyle,fg= "#747780",bg = "white")
         self.inputPrompt.pack()
     
-        self.input_owl_entry = Entry(self.frame, width = 30,borderwidth = 5,highlightbackground="white", fg = "#18453b",font = "Verdana 10 bold")
+        self.input_owl_entry = Entry(self.input_frame, width = 30,borderwidth = 5,highlightbackground="white", fg = "#18453b",font = "Verdana 10 bold")
         self.input_owl_entry.pack()
         self.input_owl_entry.insert(0,"cpsframework-v3-base.owl")
 
-        self.loadOntology = tk.Button(self.frame, text = "Load Ontology",padx = 10, pady = 5, bg = "#18453b", fg = "white",borderwidth = 5,command = self.load_ontology)
+        #button to load ontology, calls function which handles loading
+        self.loadOntology = tk.Button(self.input_frame, text = "Load Ontology",padx = 10, pady = 5, bg = "#18453b", fg = "white",borderwidth = 5,command = self.load_ontology)
         self.loadOntology.pack()
         
-        self.add_space(self.frame,"white","medium")
+        self.add_space(self.input_frame,"white","medium")
 
-
-        
-        self.concernNamePrompt = Label(self.frame, text = "Concern Name", font = fontStyle,fg= "#747780",bg = "white")
+        #set up prompt/Entry for new concern name
+        self.concernNamePrompt = Label(self.input_frame, text = "Concern Name", font = fontStyle,fg= "#747780",bg = "white")
         self.concernNamePrompt.pack()
 
-        self.concern_name_entry = Entry(self.frame, width = 30,borderwidth = 5,highlightbackground="white", fg = "#18453b",font = "Verdana 10 bold")
+        self.concern_name_entry = Entry(self.input_frame, width = 30,borderwidth = 5,highlightbackground="white", fg = "#18453b",font = "Verdana 10 bold")
         self.concern_name_entry.pack()
         self.concern_name_entry.insert(1,"testConcern")
         
-        self.subconcernOfNamePrompt = Label(self.frame, text = "Subconcern of", font = fontStyle,fg= "#747780",bg = "white")
+        #set up prompt/option menu for which concern new concern will be a subconcern of
+        self.subconcernOfNamePrompt = Label(self.input_frame, text = "Subconcern of", font = fontStyle,fg= "#747780",bg = "white")
         self.subconcernOfNamePrompt.pack()
-        self.subconcern_of_name_entry = Entry(self.frame, width = 30,borderwidth = 5,highlightbackground="white", fg = "#18453b",font = "Verdana 10 bold")
-        self.subconcern_of_name_entry.pack()
-        self.subconcern_of_name_entry.insert(1,"Trustworthiness")
+       
+        self.subconcernOf = StringVar()
         
-        self.addConcern = tk.Button(self.frame, text = "Add Concern",padx = 10, pady = 5, bg = "#18453b", fg = "white",borderwidth = 5,command = self.add_concern)
+        self.subconcern_of_name_drop = OptionMenu(self.input_frame,self.subconcernOf,None)
+        self.subconcern_of_name_drop.config(width=30, font = selectFont,fg= "#18453b",bg = "white",borderwidth = 3,highlightbackground="white")
+        self.subconcern_of_name_drop.pack()
+        
+        #button to add concern, calls function which handles it
+        self.addConcern = tk.Button(self.input_frame, text = "Add Concern",padx = 10, pady = 5, bg = "#18453b", fg = "white",borderwidth = 5,command = self.add_concern)
         self.addConcern.pack()
         
+        self.add_space(self.input_frame,"white","medium")
         
-        self.add_space(self.frame,"white","medium")
-        
-        self.propertyNamePrompt = Label(self.frame, text = "Property Name", font = fontStyle,fg= "#747780",bg = "white")
+        #set up prompt/entry for new property's name
+        self.propertyNamePrompt = Label(self.input_frame, text = "Property Name", font = fontStyle,fg= "#747780",bg = "white")
         self.propertyNamePrompt.pack()
 
-        self.property_name_entry = Entry(self.frame, width = 30,borderwidth = 5,highlightbackground="white", fg = "#18453b",font = "Verdana 10 bold")
+        self.property_name_entry = Entry(self.input_frame, width = 30,borderwidth = 5,highlightbackground="white", fg = "#18453b",font = "Verdana 10 bold")
         self.property_name_entry.pack()
         self.property_name_entry.insert(1,"testProperty")
         
-        self.concernAddressedNamePrompt = Label(self.frame, text = "Addresses Concern", font = fontStyle,fg= "#747780",bg = "white")
-        self.concernAddressedNamePrompt.pack()
-        self.concern_addressed_name_entry = Entry(self.frame, width = 30,borderwidth = 5,highlightbackground="white", fg = "#18453b",font = "Verdana 10 bold")
-        self.concern_addressed_name_entry.pack()
-        self.concern_addressed_name_entry.insert(1,"Trustworthiness")
         
-        self.addProperty = tk.Button(self.frame, text = "Add Property",padx = 10, pady = 5, bg = "#18453b", fg = "white",borderwidth = 5,command = self.add_property)
+        #set up prompt/option menu for which concern the new property will address
+        self.concernAddressedNamePrompt = Label(self.input_frame, text = "Addresses Concern", font = fontStyle,fg= "#747780",bg = "white")
+        self.concernAddressedNamePrompt.pack()
+    
+        self.addressedConcern = StringVar()
+        
+        self.addressedConcern_drop = OptionMenu(self.input_frame,self.addressedConcern,None)
+        self.addressedConcern_drop.config(width=30, font = selectFont,fg= "#18453b",bg = "white",borderwidth = 3,highlightbackground="white")
+        self.addressedConcern_drop.pack()
+        
+        #set up button for adding proerty, calls function which handles it
+        self.addProperty = tk.Button(self.input_frame, text = "Add Property",padx = 10, pady = 5, bg = "#18453b", fg = "white",borderwidth = 5,command = self.add_property)
         self.addProperty.pack()
         
         
-        self.add_space(self.frame,"white","medium")
+        self.add_space(self.input_frame,"white","medium")
 
-
-
-
-        self.outputPrompt = Label(self.frame, text =  "Output Name", font = fontStyle,fg= "#747780",bg = "white")
+        #sets up prompt/entry for name of output owl file
+        self.outputPrompt = Label(self.input_frame, text =  "Output Name", font = fontStyle,fg= "#747780",bg = "white")
         self.outputPrompt.pack()
         
-        self.output_owl_entry = Entry(self.frame, width = 30,borderwidth = 5,highlightbackground="white", fg = "#18453b",font = "Verdana 10 bold")
+        self.output_owl_entry = Entry(self.input_frame, width = 30,borderwidth = 5,highlightbackground="white", fg = "#18453b",font = "Verdana 10 bold")
         self.output_owl_entry.pack()
         self.output_owl_entry.insert(2, "newConcern.owl")
         
-        self.saveOntology = tk.Button(self.frame, text = "Output Ontology",padx = 10, pady = 5, bg = "#18453b", fg = "white",borderwidth = 5,command = self.save_ontology)
+        #sets up button to call function which handles saving ontology
+        self.saveOntology = tk.Button(self.input_frame, text = "Output Ontology",padx = 10, pady = 5, bg = "#18453b", fg = "white",borderwidth = 5,command = self.save_ontology)
         self.saveOntology.pack()
         
-        self.textBox = Frame(self.frame,bg = "#747780", bd = 5)
+        #sets up gray box to put text to show what is going on
+        self.textBox = Frame(self.input_frame,bg = "#747780", bd = 5)
         self.textBox.place(relwidth = .8, relheight = .2, relx = .1, rely = .75)
         
-
+        #sets up frame for ontology tree to exist
         self.tree_frame = tk.Frame(self.master, bg="white")
         self.tree_frame.place(relwidth = .70, relheight = .8, relx = .25, rely = 0.1)
 
@@ -119,35 +133,57 @@ class OntologyGUI:
         self.ax.axis('off')
         self.chart.get_tk_widget().pack()
 
-        self.slider_frame = tk.Frame(self.tree_frame,bg = "white")
-        self.slider_frame.place(relwidth = .7, relheight = .05, relx = .15, rely = .95)
+        
+        #set up sliders/zoom button in tree frame
+        self.xslider_frame = tk.Frame(self.tree_frame,bg = "white")
+        self.xslider_frame.place(relwidth = .7, relheight = .05, relx = .15, rely = .95)
 
-        self.xslider = Scale(self.slider_frame, from_ = 0, to = 80,orient = HORIZONTAL,bg = "gray", fg = "white",length = 900,command = self.scale_tree)     
-        self.xslider.pack()
+        self.xslider = Scale(self.xslider_frame, from_ = 0, to = 80,orient = HORIZONTAL,bg = "gray", fg = "white",length = 900,command = self.scale_tree)     
+        self.xslider.pack()    
+
+
+        self.yslider_frame = tk.Frame(self.tree_frame,bg = "white")
+        self.yslider_frame.place(relwidth = .03, relheight = .7, relx = .95, rely = .15)
+
+        self.yslider = Scale(self.yslider_frame, from_ = 50, to = 0,orient = VERTICAL,bg = "gray", fg = "white",length = 900,command = self.scale_tree)     
+        self.yslider.pack()
+        
+        
+        self.zoom_frame = tk.Frame(self.tree_frame,bg = "white")
+        self.zoom_frame.place(relwidth = .05, relheight = .05, relx = .94, rely = .01)
+        
+        self.zoom_button = tk.Button(self.zoom_frame, text = "Zoom",padx = 10, pady = 5, bg = "#18453b", fg = "white",borderwidth = 5,command = self.do_zoom)
+        self.zoom_button.pack()
+        
+        
       
         
         
-
+    #adds a concern to the ontology, uses gui entries for inputs, updates tree afterwards
     def add_concern(self):
     
+        #adds concern in
         new_concern = self.concern_name_entry.get()
        
         test_concern = self.ontology.Concern(new_concern,ontology = self.ontology)
         
-        subconcern_of = self.ontology.search(iri = "*" + self.subconcern_of_name_entry.get())
+        subconcern_of = self.ontology.search(iri = "*" + self.subconcernOf.get())
         subconcern_of = subconcern_of[0]
         
         subconcern_of.includesConcern.append(test_concern)
             
+        #prints text in textbox to tell what happend
         summary = "Added concern " + new_concern + " to ontology"
             
         self.summaryLabel = Label(self.textBox, text = summary, font = fontStyle,fg= "white",bg = "#747780")
         self.summaryLabel.pack()
-            
+             
         self.update_tree()
         
+    #adds a property to the ontology, uses gui for inputs, updates tree afterwards
     def add_property(self):
         
+        #add in the property
         new_property_name = self.property_name_entry.get()
         
         new_ir_name = self.get_ir_name()
@@ -159,14 +195,16 @@ class OntologyGUI:
         new_property.atomicStatement.append("new_atomic_statement")
         new_property.comment.append("new_comment")
         
+        #add teh condition
         new_condition = self.ontology.Condition(new_cond_name, ontology = self.ontology)
         
         new_condition.conditionPolarity.append(self.ontology.positive)
         new_condition.conditionProperty.append(new_property)
         
-        addressed_concern = self.ontology.search(iri = "*" + self.concern_addressed_name_entry.get())
+        addressed_concern = self.ontology.search(iri = "*" + self.addressedConcern.get())
         addressed_concern = addressed_concern[0]
         
+        #add the impact rule
         new_impact_rule = self.ontology.ImpactRule(new_ir_name,ontology = self.ontology)
         new_impact_rule.addressesAtFunc.append(self.ontology.bc1)
         new_impact_rule.addressesConcern.append(addressed_concern)
@@ -178,6 +216,7 @@ class OntologyGUI:
         self.no_impact_rules += 1
         self.no_conditions += 1
         
+        #prints text in text box
         summary = "Added property " + new_property_name + " to ontology"
             
         self.summaryLabel = Label(self.textBox, text = summary, font = fontStyle,fg= "white",bg = "#747780")
@@ -185,7 +224,7 @@ class OntologyGUI:
             
         self.update_tree()
         
-    
+    #returns the suitable ir name given how many already exist
     def get_ir_name(self):
         
         new_ir_num = self.no_impact_rules + 1
@@ -198,7 +237,8 @@ class OntologyGUI:
             return "ir00" + str(new_ir_num)
         if(new_ir_num >= 1):
             return "ir000" + str(new_ir_num)
-        
+    
+    #returns the suitable condition name given how many already exist
     def get_cond_name(self):
         
         new_cond_num = self.no_conditions + 1
@@ -214,14 +254,13 @@ class OntologyGUI:
         
         
         
-        
-        
+    #draws the tree and scales it according to zoom level, scroll bar positions
     def update_tree(self):
          
          self.ax.clear()
          
 
-         self.xmin, self.xmax, self.ymin, self.ymax = draw_ontology(self.ax,self.ontology)
+         self.xmin, self.xmax, self.ymin, self.ymax, self.sorted_concerns= draw_ontology(self.ax,self.ontology,self.fontsize)
          
          self.totalx = self.xmax - self.xmin
          self.totaly = self.ymax - self.ymin
@@ -232,16 +271,19 @@ class OntologyGUI:
          self.ymin = self.ymin - self.totaly/10
          self.ymax = self.ymax + self.totaly/10
          
-         
-         
-         self.ax.set(xlim=(self.xmax*(self.xslider.get()/100), self.xmax*(self.xslider.get()/100 + .2)), ylim=(self.ymin, self.ymax))
+         print(self.ymin, " self.ymin")
+         print(self.ymax, " self.ymax")
 
          
-         print(self.ax.get_xlim())
-         print(self.ax.get_ylim())
+         self.scale_tree(3)
+       
          self.chart.draw()
          
+         #updates the concern dropdowns with whatever was found in draw_ontology
+         self.update_concern_dropdown()
          
+     
+    #loads the specified ontology file in
     def load_ontology(self):
         
         self.ontology = get_ontology("file://./" + self.input_owl_entry.get()).load() 
@@ -254,8 +296,52 @@ class OntologyGUI:
         
         self.no_conditions = len(self.ontology.search(type = self.ontology.Condition))
         self.no_impact_rules =  len(self.ontology.search(type = self.ontology.ImpactRule))
+        
     
-     
+    
+    
+    #updates the dropdowns containing the list of concerns, keeps whatever is currently select3ed
+    def update_concern_dropdown(self):
+        
+        subcof = self.subconcernOf.get()
+        addC = self.addressedConcern.get()
+        
+        
+        self.subconcern_of_name_drop.children["menu"].delete(0,"end")
+        self.addressedConcern_drop.children["menu"].delete(0,"end")
+        i = 0
+        j = 0
+        k = 0
+        for node in self.sorted_concerns:
+            if(node == subcof):
+                j = i
+            if(node == addC):
+                k = i
+            self.subconcern_of_name_drop.children["menu"].add_command(label = node, command = lambda name = node: self.subconcernOf.set(name))
+            self.addressedConcern_drop.children["menu"].add_command(label = node, command = lambda name = node: self.addressedConcern.set(name))
+            
+            i = i + 1
+        self.subconcernOf.set(self.sorted_concerns[j])
+        self.addressedConcern.set(self.sorted_concerns[k])
+        
+    #changes zoom level, fontsize, then calls update_tree to draw it again
+    def do_zoom(self):
+        
+        print("we be zooming")
+        
+        if(self.zoom == 1):
+            self.zoom = 2
+            self.fontsize = 16
+            
+        elif(self.zoom == 2):
+            self.zoom = 1
+            self.fontsize = 8
+        
+        self.update_tree()
+        
+        
+        
+    #saves the ontology in rdf format
     def save_ontology(self):
          
         output_file = self.output_owl_entry.get()
@@ -269,18 +355,24 @@ class OntologyGUI:
         self.summaryLabel.pack()
         
         
+    #changes the portion of axis we view according to zoom level, slider position
     def scale_tree(self,var):
         
-         self.ax.set(xlim=(self.xmax*(self.xslider.get()/100), self.xmax*(self.xslider.get()/100 + .2)), ylim=(self.ymin, self.ymax))
+    
+         if (self.zoom == 1):
+             self.ax.set(xlim=(self.xmax*(self.xslider.get()/100), self.xmax*(self.xslider.get()/100 + .2)), ylim=(self.ymin, self.ymax))
          
-         print(self.ax.get_xlim())
-         print(self.ax.get_ylim())
-         print(self.ax.get_xlim()[1] - self.ax.get_xlim()[0])
+         if (self.zoom == 2):
+             self.ax.set(xlim=(self.xmax*(self.xslider.get()/100 + .05), self.xmax*(self.xslider.get()/100 + .15)), ylim=(self.ymax*(self.yslider.get()/100 + .001)  , self.ymax*(self.yslider.get()/100 + .5)))
+
+         #print(self.ax.get_xlim(), " in scale")
+         #print(self.ax.get_ylim(), " in scale")
+         #print(self.ax.get_xlim()[1] - self.ax.get_xlim()[0])
          self.chart.draw()
         
         
 
-
+    #function to add space in specified location, with specified color
     def add_space(self,on,color,size):
         
         if(size == "small"):
@@ -295,9 +387,9 @@ class OntologyGUI:
         self.emptySpace.pack()
         
         
+    #handles processing the output file after we save it, for now just strips string tag in owl file.
     def process_file(self,output_file):
     
-
         #read input file
         fin = open(output_file, "rt")
         #read file contents to string
@@ -322,9 +414,10 @@ class OntologyGUI:
 
     
 root = Tk()
-fontStyle = tkFont.Font(family="Lucida Grande", size=6, weight = "bold")
+fontStyle = tkFont.Font(family="Lucida Grande", size=8, weight = "bold")
 small = tkFont.Font(family="Lucida Grande", size=6, weight = "bold")
 med = tkFont.Font(family="Lucida Grande", size=12, weight = "bold")
 big = tkFont.Font(family="Lucida Grande", size=18, weight = "bold") 
+selectFont = tkFont.Font(family="Lucida Grande", size=11, weight = "bold")
 my_gui = OntologyGUI(root)
 root.mainloop()
--- interface/testing/script_networkx.py
+++ interface/testing/script_networkx.py
@@ -9,8 +9,6 @@ my_onto = get_ontology("file://./cpsframework-v3-base.owl").load()
 
 
 
-
-
 #removes the namespace from an owlr object, turns it into a string
 def remove_namespace(in_netx):
    
@@ -25,7 +23,7 @@ def remove_namespace(in_netx):
  
     return in_str[(period + 1):]
     
-#recursive function to retrieve all concerns
+#recursive function to retrieve all concerns, puts them in concern_list
 def get_all_concerns(T,concern,concern_list):
     
     #appends to the total concern list
@@ -45,7 +43,10 @@ def draw_children(G,node):
         G.add_edge(remove_namespace(node),remove_namespace(child),length = 1)
         
 
-def add_properties(G,ontology,property_list,concerns_list,color_map):
+#adds properties to the graph by finding all impact rules, finding the concern they address, then the property they are connected to 
+def add_properties(G,ontology,property_list):
+    
+    concerns_list = []
     
     impact_rules = ontology.search(type = ontology.ImpactRule)
     
@@ -56,42 +57,28 @@ def add_properties(G,ontology,property_list,concerns_list,color_map):
     
     for ir in impact_rules:
         
-        property_list.append(ir.hasCondition[0].conditionProperty[0])
+        property_list.append(remove_namespace(ir.hasCondition[0].conditionProperty[0]))
         
         
-        
-    #print(property_list)
-    #print(concerns_list)
- 
     for i in range(len(property_list)):
+           
+        G.add_node(property_list[i])
         
-       # if(remove_namespace(property_list[i]) == "Input1ConsistentReadingFreq" or remove_namespace(property_list[i]) == "Input1Modes"):
-       #     continue
-        
-        G.add_node(remove_namespace(property_list[i]))
-        
-        
-        color_map.append("purple")
-        
-        G.add_edge(remove_namespace(concerns_list[i]),remove_namespace(property_list[i]))
-        
+        G.add_edge(remove_namespace(concerns_list[i]),property_list[i])
+      
+         
+def draw_ontology(ax,cps_onto,fs):
     
-
-    
+    ax.axis('off')
         
-        
-def draw_ontology(ax,cps_onto):
-    
-
     T = nx.DiGraph()
 
     all_nodes = []
-    addressed_concerns = []
+    all_concerns = []
     myproperty_list = []
     
-    color_map = []
-    shape_map = []
-    bbox_map = []
+    
+
 
     all_aspects = cps_onto.search(type = cps_onto.Aspect)
     #all_aspects = [cps_onto.Trustworthiness]
@@ -101,82 +88,38 @@ def draw_ontology(ax,cps_onto):
     
         get_all_concerns(T, aspect, all_nodes)
 
-    #print(all_nodes)
-
     #adds all nodes into graph
     for node in all_nodes:
         T.add_node(remove_namespace(node))
         
         node_type = remove_namespace(node.is_a[0])
-        
-        if(node_type == "Aspect"):
-            color_map.append("blue")
-            shape_map.append("sq")
-            
-            
-        elif(node_type == "Concern"):
-            color_map.append("red")
-            
-    
-        
-        
-    #draws edges between all nodes
+
+    #draws edges between all concerns
     for node in all_nodes:
         
         draw_children(T,node)
     
-    add_properties(T,cps_onto,myproperty_list,addressed_concerns,color_map)
+    #adds all properties and their edges to graph
+    add_properties(T,cps_onto,myproperty_list)
     
-    #does positioning stuff
-    write_dot(T,'test.dot')    
-    pos = graphviz_layout(T, prog='dot')#, args = "-Goverlap=scale -Nfixed=true -Nwidth=5 -Nheight=5" )
+    #let graphviz handle creating tree structure
+    pos = graphviz_layout(T, prog='dot')
     
-    
-    x_pos = np.array(())
-    y_pos = np.array(())
-    
-    #print(pos.keys())
-    #i = 0
-    for x in pos:
-        
-        print (pos[x])
-        
-        position = pos[x]
-        
-        x_pos = np.append(x_pos,position[0])
-        y_pos = np.append(y_pos,position[1])
-        
-        
-        
-    xmax = np.max(x_pos)
-    ymax = np.max(y_pos)
-    
-    xmin = np.min(x_pos)
-    ymin = np.min(y_pos)
-    
-    print("maxes")
-    print(xmin, " ", xmax)
-    print(ymin, " ", ymax)
-        #print (pos[x])
-        
-    
-    
-    x_pos = np.array(())
-    y_pos = np.array(())
-    
-    
-    print(x_pos)
+    #don't want negative position values, so add 500 to all x's, 100 to all y's
     for x in pos:
     
             lst = list(pos[x])
             lst[0] = lst[0] + 500
+            lst[1] = lst[1] + 100
             
             pos[x] = tuple(lst)
-            
-            
+      
+    #find x,y mins and maxes for gui graphing purposes
+    x_pos = np.array(())
+    y_pos = np.array(())
+                       
     for x in pos:
         
-        print(pos[x])
         position = pos[x]
         x_pos = np.append(x_pos,position[0])
         y_pos = np.append(y_pos,position[1])
@@ -188,28 +131,48 @@ def draw_ontology(ax,cps_onto):
     
     xmin = np.min(x_pos)
     ymin = np.min(y_pos)
-    
-    
-    print(x_pos)
     
     print("maxes")
     print(xmin, " ", xmax)
     print(ymin, " ", ymax)
                
-    
-            
-        
+    #sort the concerns 
+    for node in all_nodes:
+        all_concerns.append(remove_namespace(node))
    
-
-    
+    sorted_concerns = sorted(all_concerns, key=lambda x:x.upper())
     
     plt.tight_layout()
-    nx.draw(T, pos, with_labels=True, arrows=True, ax = ax, node_size = .1, font_size = 8,node_shape = "s", bbox=dict(facecolor="blue", edgecolor='black', boxstyle='round,pad=0.2'),font_color = "white",scale = 1)
+    #nx.draw(T, pos, with_labels=True, arrows=True, ax = ax, node_size = .1, font_size = fs,node_shape = "s", bbox=dict(facecolor="#737373", boxstyle='round,pad=0.2'),font_color = "white",scale = 1)
     
-    return xmin, xmax, ymin, ymax
+    #draw all of the nodes, with no labels
+    nx.draw_networkx_nodes(T, pos, with_labels=False, arrows=False, ax = ax, node_size = .1,scale = 1)
+    #draw all of the edges
+    nx.draw_networkx_edges(T, pos, arrows=False)
+    
+    #draw all of the concerns with one color, the properties with another
+    labels = {}
+    
+    for concern in sorted_concerns:
+        labels[concern] = concern
+        
+    nx.draw_networkx_labels(T,pos,labels,font_size=fs,bbox=dict(facecolor="#737373", boxstyle='round,pad=0.2'),font_color = "white")   
+    
+
+    labels = {}
+    
+    for myproperty in myproperty_list:
+        
+        labels[myproperty] = myproperty
+        
+    nx.draw_networkx_labels(T,pos,labels,font_size=fs,bbox=dict(facecolor="red", boxstyle='round,pad=0.2'),font_color = "white") 
+    
+    
+    
+    return xmin, xmax, ymin, ymax, sorted_concerns
     
 fig, ax = plt.subplots(figsize = (10,10))
 
-draw_ontology(ax,my_onto)
+draw_ontology(ax,my_onto,8)
 
 
