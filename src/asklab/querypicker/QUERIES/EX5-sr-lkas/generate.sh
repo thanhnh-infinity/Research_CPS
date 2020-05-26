38ad14f3722107131098074221e75df4282881eb
thanhnh-infinity <thanhnh2911@gmail.com>
2020-05-26 07:14:23 -0600

    restructure sophisticated reasoning

---
+++ src/asklab/querypicker/QUERIES/EX5-sr-lkas/generate.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+
+BASE=../BASE/
+
+REASONING=../Sophisticated-Reasoning/
+
+#FILES="$BASE/step1-BASE.lp $BASE/theory.lp $BASE/theory-maxint.lp output.lp $BASE/step1.lp"
+#OUTPUT=FULL-step1.txt
+#cat $FILES > $OUTPUT
+
+#FILES="$BASE/step1-BASE.lp $BASE/theory.lp $BASE/theory-maxint.lp output.lp $BASE/step1.lp $BASE/step2.lp"
+#OUTPUT=FULL-step2.txt
+#cat $FILES > $OUTPUT
+
+#FILES="$BASE/step1-BASE.lp $BASE/theory.lp $BASE/theory-maxint.lp output.lp $BASE/step1.lp $BASE/step2.lp $BASE/step3.lp"
+#OUTPUT=FULL-step3.txt
+#cat $FILES > $OUTPUT
+
+#FILES="$BASE/step1-BASE.lp $BASE/theory.lp $BASE/theory-maxint.lp output.lp $BASE/step1.lp $BASE/step2.lp $BASE/step3.lp $BASE/step3-ext.lp"
+#OUTPUT=FULL-step3-ext.txt
+#cat $FILES > $OUTPUT
+
+
+FILES="$REASONING/Integration/SR-RE-01-truthworthiness/ontology_LKAS_2.lp $REASONING/Integration/SR-RE-01-truthworthiness/reasoning_2_1.lp output_sr_re_01_01.lp"
+OUTPUT=FULL-step1.txt
+cat $FILES > $OUTPUT
+
+FILES="$REASONING/Integration/SR-RE-01-truthworthiness/ontology_LKAS_2.lp $REASONING/Integration/SR-RE-01-truthworthiness/reasoning_2_2.lp output_sr_re_01_02.lp"
+OUTPUT=FULL-step2.txt
+cat $FILES > $OUTPUT
+
+FILES="$BASE/step1-BASE.lp $BASE/theory.lp $BASE/theory-maxint.lp output.lp $BASE/step1.lp $BASE/step2.lp"
+OUTPUT=FULL-step3.txt
+cat $FILES > $OUTPUT
+
+FILES="$BASE/step1-BASE.lp $BASE/theory.lp $BASE/theory-maxint.lp output.lp $BASE/step1.lp $BASE/step2.lp $BASE/step3.lp"
+OUTPUT=FULL-step4.txt
+cat $FILES > $OUTPUT
+
+FILES="$BASE/step1-BASE.lp $BASE/theory.lp $BASE/theory-maxint.lp output.lp $BASE/step1.lp $BASE/step2.lp $BASE/step3.lp $BASE/step3-ext.lp"
+OUTPUT=FULL-step4-ext.txt
+cat $FILES > $OUTPUT
