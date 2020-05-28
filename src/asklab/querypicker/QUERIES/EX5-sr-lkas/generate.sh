f28f30cb6b1cd8e20f8d0985be42173104358c51
Thanh Nguyen <thanhnh2911@gmail.com>
2020-05-28 10:05:42 -0600

    reconfig

--- src/asklab/querypicker/QUERIES/EX5-sr-lkas/generate.sh
+++ src/asklab/querypicker/QUERIES/EX5-sr-lkas/generate.sh
@@ -21,11 +21,11 @@ REASONING=../Sophisticated-Reasoning/
 #cat $FILES > $OUTPUT
 
 
-FILES="$REASONING/Integration/SR-RE-01-truthworthiness/ontology_LKAS_2.lp $REASONING/Integration/SR-RE-01-truthworthiness/reasoning_2_1.lp output_sr_re_01_01.lp"
+FILES="$REASONING/Integration/SR-RE-01-truthworthiness/ontology_LKAS_2.lp $REASONING/Integration/SR-RE-01-truthworthiness/obs_init_state_LKAS.lp $REASONING/Integration/SR-RE-01-truthworthiness/reasoning_2_1.lp output_sr_re_01_01.lp"
 OUTPUT=FULL-step1.txt
 cat $FILES > $OUTPUT
 
-FILES="$REASONING/Integration/SR-RE-01-truthworthiness/ontology_LKAS_2.lp $REASONING/Integration/SR-RE-01-truthworthiness/reasoning_2_2.lp output_sr_re_01_02.lp"
+FILES="$REASONING/Integration/SR-RE-01-truthworthiness/ontology_LKAS_2.lp $REASONING/Integration/SR-RE-01-truthworthiness/obs_init_state_LKAS.lp $REASONING/Integration/SR-RE-01-truthworthiness/reasoning_2_2.lp output_sr_re_01_02.lp"
 OUTPUT=FULL-step2.txt
 cat $FILES > $OUTPUT
 
@@ -33,10 +33,10 @@ FILES="$BASE/step1-BASE.lp $BASE/theory.lp $BASE/theory-maxint.lp output.lp $BAS
 OUTPUT=FULL-step3.txt
 cat $FILES > $OUTPUT
 
-FILES="$BASE/step1-BASE.lp $BASE/theory.lp $BASE/theory-maxint.lp output.lp $BASE/step1.lp $BASE/step2.lp $BASE/step3.lp"
+FILES="$REASONING/Integration/SR-RE-01-truthworthiness/ontology_LKAS_2.lp $REASONING/Integration/SR-RE-01-truthworthiness/obs_init_state_LKAS.lp $REASONING/Integration/SR-RE-01-truthworthiness/reasoning_2_3.lp output_sr_re_01_03.lp"
 OUTPUT=FULL-step4.txt
 cat $FILES > $OUTPUT
 
-FILES="$BASE/step1-BASE.lp $BASE/theory.lp $BASE/theory-maxint.lp output.lp $BASE/step1.lp $BASE/step2.lp $BASE/step3.lp $BASE/step3-ext.lp"
+FILES="$REASONING/Integration/SR-RE-01-truthworthiness/ontology_LKAS_2.lp $REASONING/Integration/SR-RE-01-truthworthiness/obs_init_state_LKAS.lp $REASONING/Integration/SR-RE-01-truthworthiness/reasoning_2_4.lp output_sr_re_01_04.lp"
 OUTPUT=FULL-step4-ext.txt
 cat $FILES > $OUTPUT
--- src/asklab/querypicker/QUERIES/generate-all-lp-files.sh
+++ src/asklab/querypicker/QUERIES/generate-all-lp-files.sh
@@ -2,7 +2,7 @@
 
 DIRS="EX0-general EX0-optimistic EX0-pessimistic EX0-pessimistic-prob \
       EX0-related-req EX0-sake-of-concern EX0-skeptical \
-      EX1-camera EX2-laneassist EX3-adaptivecruise EX4-elevator EX4-sr-elevator"
+      EX1-camera EX2-laneassist EX3-adaptivecruise EX4-elevator EX4-sr-elevator EX5-sr-lkas"
 
 for d in $DIRS
 do
