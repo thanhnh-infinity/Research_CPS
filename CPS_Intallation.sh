78763f65596c246637252af0cfed5244015233c7
thanhnh-infinity <thanhnh2911@gmail.com>
2022-11-12 20:11:18 -0700

    Add installation and running file

--- CPS_Intallation.sh
+++ CPS_Intallation.sh
@@ -20,6 +20,17 @@ if [[ "$_java" ]]; then
            javac -version
            javac -d bin/ -cp src src/asklab/querypicker/QueryPicker.java
            javac -d bin/ -cp src src/asklab/cpsf/CPSReasoner.java
+           cp ./src/asklab/querypicker/dump.sparql ./bin/asklab/querypicker
+           cp -r ./src/asklab/querypicker/QUERIES ./bin/asklab/querypicker/
+           cp -r ./src/asklab/cpsf/clingo-4.4.0 ./bin/asklab/cpsf/
+           cp -r ./src/asklab/cpsf/dlv ./bin/asklab/cpsf/
+           cp -r ./src/asklab/cpsf/mkatoms ./bin/asklab/cpsf/
+           cp -r ./src/asklab/cpsf/apache-jena-3.0.0 ./bin/asklab/cpsf/
+           cp ./src/asklab/cpsf/runjena.bat ./bin/asklab/cpsf/
+           cp ./src/asklab/cpsf/runjena.sh ./bin/asklab/cpsf/
+           cp ./src/asklab/cpsf/version.txt ./bin/asklab/cpsf/ 
+           echo "----- Done : Install CPS successfully ----- "
+           echo "Using command: $ ./runCPS.sh to run the application"
         else
            echo "You don't have GIT in your machine. Please install GIT. On MacOS : brew install git, On Linux: sudo apt-get install git"
         fi
